0019cba6175e306e9e629fe5147d93990b512bf9
Khurtis <nii.codjoe@gmail.com>
2023-02-27 11:10:55 -0800

    puts.c file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * _puts - prints a string, followed by a new line.
  * @str: input string.
  * Return: no return.
  */
-void _putchar(char *str)
+void _puts(char *str)
 {
 	int count = 0;
 
